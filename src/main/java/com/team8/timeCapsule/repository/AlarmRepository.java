90d4e2ebb812a7f7a0bd494774b534b00e6b7107
김규리 <kimkyuree1@naver.com>
2024-09-28 22:50:13 +0900

    feat: add alarm database

---
+++ src/main/java/com/team8/timeCapsule/repository/AlarmRepository.java
@@ -0,0 +1,12 @@
+package com.team8.timeCapsule.repository;
+
+import com.team8.timeCapsule.domain.Alarm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AlarmRepository extends JpaRepository<Alarm, Long> {
+    // unlockDate가 현재 시각 이전이면서 확인되지 않은 알람 조회
+    List<Alarm> findAllByUnlockDateLessThanEqualAndIsConfirmFalse(LocalDateTime currentTime);
+}
