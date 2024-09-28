992693df501a37be8649d6ca0fe6965b4726c53f
김규리 <kimkyuree1@naver.com>
2024-09-28 16:22:03 +0900

    feat: alarm api 추가

--- src/main/java/com/team8/timeCapsule/service/AlarmService.java
+++ src/main/java/com/team8/timeCapsule/service/AlarmService.java
@@ -5,11 +5,32 @@ import com.team8.timeCapsule.repository.AlarmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class AlarmService {
 
     @Autowired
     private AlarmRepository alarmRepository;
+
+    // 알람 목록 조회 메소드
+    public List<Alarm> getAllAlarms() {
+        return alarmRepository.findAll();
+    }
+
+    // 알람 확인 여부 수정 메소드
+    public Alarm confirmAlarm(Long alarmId) {
+        Optional<Alarm> optionalAlarm = alarmRepository.findById(alarmId);
+
+        if (optionalAlarm.isPresent()) {
+            Alarm alarm = optionalAlarm.get();
+            alarm.setIsConfirm(true);  // isConfirm을 true로 설정
+            return alarmRepository.save(alarm);  // 변경사항 저장
+        } else {
+            throw new RuntimeException("Alarm not found with id: " + alarmId);
+        }
+    }
 
     public Alarm createAlarm(String title, String content, Long timeCapsuleId) {
         Alarm alarm = new Alarm();
