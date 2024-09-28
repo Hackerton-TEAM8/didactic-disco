1d9fa81a5c0c26f0498583de7f736bbbce4b1086
dfdfg42 <dfdfg11@gmail.com>
2024-09-28 14:28:44 +0900

    feat: 로그인 초기세팅 / 유저엔티티 확정

---
+++ src/main/java/com/team8/timeCapsule/security/TokenProvider.java
@@ -0,0 +1,55 @@
+package com.team8.timeCapsule.security;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import com.team8.timeCapsule.domain.UserEntity;
+import org.springframework.stereotype.Service;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class TokenProvider {
+    private static final String SECRET_KEY = "NMA8JPctFuna59f5";
+
+    public String create(UserEntity userEntity) {
+        Date expireDate = Date.from(
+                Instant.now().plus(1, ChronoUnit.DAYS));
+
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .setSubject(userEntity.getId())
+                .setIssuer("todo app")
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .compact();
+    }
+
+    public String create(String userId) {
+        Date expireDate = Date.from(
+                Instant.now().plus(1, ChronoUnit.DAYS));
+
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .setSubject(userId)
+                .setIssuer("todo app")
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .compact();
+    }
+
+    public String validateAndGetUserId(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
